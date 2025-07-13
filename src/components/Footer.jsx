66d3f3cb20583d1c654043df5d7fd94d0b19be4f
Michal Szyszlo <mszyszlo@student.agh.edu.pl>
2025-07-13 21:20:52 +0200

    Footer - elementy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,31 +10,51 @@ export default function Footer(){
         agh_logo,
         umcs_logo
     ];
-    const ref = useRef(null);
-    const x = useRef(0);
-
-    useAnimationFrame((time, delta) => {
-        if (ref.current) {
-            x.current -= (delta / 1000) * 100; // 100px per second
-            if (x.current <= -ref.current.offsetWidth / 2) {
-                x.current = 0; // reset to start
-            }
-            ref.current.style.transform = `translateX(${x.current}px)`;
-        }
-    })
 
     return <>
-        <div className="overflow-hidden w-full h-[50vh]">
-            <div ref={ref} className="flex whitespace-nowrap">
+        <div className="text-white pl-6 pt-4 overflow-hidden whitespace-nowrap w-full h-[100vh] bg-[#3077BA]">
+            <p className="mb-5">Nasze współprace</p>
+            <motion.div
+                className="flex"
+                animate={{ x: "-122%" }} // tylko %, bez calc
+                transition={{
+                    duration: 10,
+                    ease: "linear",
+                    repeat: Infinity,
+                }}
+            >
                 {[...images, ...images].map((src, i) => (
-                    <img
+                    <><img
                         key={i}
                         src={src}
                         alt={`img-${i}`}
-                        className="h-48 w-auto object-cover mx-4"
+                        className="h-20 w-auto object-cover mx-4"
                     />
+                        <div className="border-l-2 border-gray-300 h-10 translate-y-5"></div></>
+
                 ))}
+            </motion.div>
+            <div className="flex flex-col">
+                <h1 className="text-lg mt-5 mb-3">Kontakt</h1>
+                <p className="text-md mb-1">+84 639 28 01</p>
+                <p className="text-md">sekretariat@1lo.zamosc.pl</p>
             </div>
+            <h1 className="text-xl mt-5 mb-3">Menu</h1>
+            <div className="flex gap-8">
+                <div className="flex flex-col gap-3">
+                    <p className="text-md">Deklaracja dostępności strony</p>
+                    <p className="text-md">Polityka prywatności</p>
+                    <p className="text-md">RODO</p>
+                    <p className="text-md">BIP</p>
+                </div>
+                <div className="flex flex-col gap-3">
+                    <p className="text-md">Plany lekcji</p>
+                    <p className="text-md">E-dziennik</p>
+                    <p className="text-md">Stypendia</p>
+                    <p className="text-md">Certyfikaty</p>
+                </div>
+            </div>
+            <span></span>
         </div>
     </>
 }
