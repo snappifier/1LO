4e8895bb61d770258d6a5a3c1a6a99a38d5a92ab
snappify <nzzhry@gmail.com>
2025-07-14 16:27:09 +0200

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,88 @@
-import {motion} from "motion/react"
-import {Link} from "react-router-dom"
+import logo from "../assets/logo.png";
+import logoblack from "../assets/logoonlyblack.png";
+import { ExitIcon } from "./animations/ExitIcon";
+
+import {useState, useEffect} from "react";
+import {motion, AnimatePresence} from "motion/react";
+import {Link} from "react-router-dom";
 
 export const Navbar = () => {
 
-    return(<>
-        {/*<header className="sticky top-0 left-0 z-50 w-full h-30 bg-black/65 backdrop-saturate-300 shadow-md">*/}
-        {/*    <nav className="flex justify-between items-center max-w-7xl mx-auto px-4 py-3">*/}
-        {/*    <Link to={"/"} className="focus-visible:ring-3 focus-visible:ring-sky-600 focus-visible:ring-offset-2 focus-visible:ring-offset-transparent focus-visible:rounded-[50%]" >*/}
-        {/*    <motion.div*/}
-        {/*        className="cursor-pointer select-none border-4 border-white rounded-[50%] size-24 shadow-md flex items-center justify-center text-white text-xl"*/}
-        {/*        whileHover={{scale: 1.1}}*/}
-        {/*        whileTap={{scale: 1}}*/}
+    const [scrollY, setScrollY] = useState(0);
+    const [scrolled, setScrolled] = useState(false);
+    const [navOpen, setNavOpen] = useState(false);
 
 
+    const navLinks = [
+        {name: 'Home', path: '/'},
+        {name: 'Szkola', path: '/szkola'},
+    ]
 
-        {/*    >Logo*/}
-        {/*    </motion.div>*/}
-        {/*    </Link>*/}
-        {/*    </nav>*/}
-        {/*</header>*/}
-            <header className="sticky top-0 w-full h-30 bg-gradient-to-b from-black/65  to-black/30 backdrop-saturate-300 shadow-sm shadow-black/30">
 
-            </header>
-        </>
-    );
+    const HEADER_HEIGHT = 110; //wysokosc zmiany navbarow
+
+    const handleScroll = () => {
+        const currentY = window.scrollY;
+        if (currentY > HEADER_HEIGHT) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+        setScrollY(currentY);
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [scrollY]);
+
+    return (<>
+        <header className={`top-0 w-full z-50 flex justify-between items-center ${scrolled
+            ? "h-20 fixed bg-white shadow-md"
+            : "h-30 absolute bg-gradient-to-b from-black/80  to-transparent "}`}
+        >
+            <Link to={'/'} className="z-[60]">
+            <div className={`flex items-center font-[golos_text] leading-none drop-shadow-sm ${scrolled ? "text-sm text-black" : "text-sm text-white"}`}>
+                <motion.img
+                    src={scrolled ? logoblack : logo}
+                    className={` drop-shadow-md ${scrolled ? "ml-4 mr-3 h-18 rounded-full" : "m-3 h-20"}`}
+                    whileHover={{scale: 1.1}}
+                    whileTap={{scale: 1}}
+                />
+                <div>
+                    <p>I Liceum</p>
+                    <p>Ogólnokształcące im.</p>
+                    <p>Jana Zamoyskiego</p>
+                </div>
+
+            </div>
+        </Link>
+            <motion.button
+                onClick={() => setNavOpen(!navOpen)}
+                className={`cursor-pointer size-12 flex items-center justify-center font-bold mx-7 z-[999] md:hidden ${scrolled ? "text-black text-2xl" : "text-white text-3xl" }`}
+
+            >
+                <ExitIcon isOpen={navOpen}/></motion.button>
+        </header>
+        <AnimatePresence>
+        {navOpen && (
+            <motion.nav
+                key="mobile-nav"
+            initial={{y: -800, opacity: 0}}
+            animate={{y: 0, opacity: 1}}
+            exit={{y: -800, opacity: 0}}
+            transition={{duration: 0.4}}
+            className="fixed top-0 left-0 w-full h-full bg-blue-900 z-40 overflow-hidden"
+
+            >
+                {navLinks.map((link) => (
+                    <Link key={link.name} to={link.path} onClick={() => setNavOpen(false)}>{link.name}</Link>
+                ))}
+            </motion.nav>
+        )};
+        </AnimatePresence>
+        <div className="absolute top-0 z-0 h-[2000px] w-full bg-zinc-900"></div>
+    </>)
 }
+
+
