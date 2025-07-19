3f4f010117b45d272ec3756637cde746b4c82fa1
Michal Szyszlo <mszyszlo@student.agh.edu.pl>
2025-07-19 21:11:19 +0200

    Banner height

--- src/sections/Home/banner.jsx
+++ src/sections/Home/banner.jsx
@@ -3,15 +3,15 @@ import baner1LO from "../../assets/Home/zdj1.jpg";
 
 export default function Banner(){
     return <>
-        <div className="w-full relative">
+        <div className="w-full h-[80vh] flex">
             <img
                 src={baner1LO}
-                className="w-full brightness-75 object-cover object-bottom"
+                className="relative w-full brightness-75 object-cover object-bottom"
                 alt="baner"
             />
 
             {/* Niebieski prostokąt - nakładka na dół zdjęcia */}
-            <div className="absolute bottom-[-40px] left-1/2 translate-x-[-50%] w-3/4 h-[80px] bg-[#3077BA] z-10"></div>
+            <div className="absolute -bottom-[calc(-20vh+40px)] left-1/2 translate-x-[-50%] w-3/4 h-[80px] bg-[#3077BA] z-10"></div>
         </div>
     </>
 }
