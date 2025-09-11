60c80fa1df41e7e828d4b0ae2a9072622626aeac
snappify <nzzhry@gmail.com>
2025-09-11 16:12:00 +0200

    podstrony

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -3,12 +3,44 @@ import Home from "./sections/Home/Home.jsx";
 import Footer from "./components/Footer.jsx";
 import {Navbar} from "./components/Navbar.jsx";
 import {Loader} from "./components/animations/Loader.jsx";
+import {Maintenance} from "./components/Maintenance.jsx";
 
 export const AppRoutes = () => {
     return (
         <Routes >
             <Route path="/" element={<><Navbar/><Home /><Footer/></>} />
-            <Route path="/kontakt" element={<><Loader/></>} />
+            <Route path="/aktualnosci" element={<Maintenance/>} />
+            <Route path="/rekrutacja" element={<Maintenance/>} />
+            <Route path="/kontakt" element={<Maintenance/>} />
+            <Route path="/certyfikaty" element={<Maintenance/>} />
+            <Route path="/deklaracja-dostepnosci" element={<Maintenance/>} />
+            <Route path="/procedury" element={<Maintenance/>} />
+            <Route path="/program-wychowawczo-profilaktyczny" element={<Maintenance/>} />
+            <Route path="/rodo" element={<Maintenance/>} />
+            <Route path="/standardy-ochrony-maloletnich" element={<Maintenance/>} />
+            <Route path="/statut" element={<Maintenance/>} />
+            <Route path="/erasmus" element={<Maintenance/>} />
+            <Route path="/wymiana-freiburg" element={<Maintenance/>} />
+            <Route path="/akademia-zamojska" element={<Maintenance/>} />
+            <Route path="/historia-szkoly" element={<Maintenance/>} />
+            <Route path="/hymny" element={<Maintenance/>} />
+            <Route path="/kalendarium" element={<Maintenance/>} />
+            <Route path="/lokalizacja" element={<Maintenance/>} />
+            <Route path="/osiagniecia" element={<Maintenance/>} />
+            <Route path="/o-szkole" element={<Maintenance/>} />
+            <Route path="/patron" element={<Maintenance/>} />
+            <Route path="/biblioteka" element={<Maintenance/>} />
+            <Route path="/dzwonki" element={<Maintenance/>} />
+            <Route path="/harmonogram-nauczycieli" element={<Maintenance/>} />
+            <Route path="/klasy" element={<Maintenance/>} />
+            <Route path="/plan-lekcji" element={<Maintenance/>} />
+            <Route path="/podreczniki" element={<Maintenance/>} />
+            <Route path="/stypendia" element={<Maintenance/>} />
+            <Route path="/zajecia-pozalekcyjne" element={<Maintenance/>} />
+            <Route path="/dyrekcja" element={<Maintenance/>} />
+            <Route path="/laureaci" element={<Maintenance/>} />
+            <Route path="/nauczyciele" element={<Maintenance/>} />
+            <Route path="/rada-rodzicow" element={<Maintenance/>} />
         </Routes>
     );
 }
---
+++ src/sections/Certyfikaty/Certyfikaty.jsx
@@ -0,0 +1,5 @@
+import {Maintenance} from "../../components/Maintenance.jsx";
+
+export const Certyfikaty = () => {
+    return (<Maintenance/>);
+}
