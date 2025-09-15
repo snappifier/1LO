83f8237e4cdc7ba1be709c712fd20544eb84d248
snappify <nzzhry@gmail.com>
2025-09-15 14:31:08 +0200

    banner

--- src/sections/Home/banner.jsx
+++ src/sections/Home/banner.jsx
@@ -7,7 +7,7 @@ export default function Banner() {
             <div className="relative w-full h-[70vh] md:h-[80vh]">
                 <img
                     src={baner1LO}
-                    className="w-full h-full brightness-55 object-cover object-bottom"
+                    className="w-full h-full brightness-55 object-cover object-bottom drop-shadow-lg/60"
                     alt="baner"
                 />
                 <p className="w-max absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3 z-20 font-[meow_script] text-5xl md:text-9xl text-white text-center">
@@ -16,7 +16,7 @@ export default function Banner() {
             </div>
 
             {/* Niebieski prostokąt - nakładka na dół zdjęcia */}
-            <div className="absolute -top-[calc(-70vh+40px)] md:-top-[calc(-80vh+40px)] left-1/2 translate-x-[-50%] w-3/4 h-[80px] bg-[#3077BA] z-10"></div>
+            {/*<div className="absolute -top-[calc(-70vh+40px)] md:-top-[calc(-80vh+40px)] left-1/2 translate-x-[-50%] w-3/4 h-[80px] bg-[#3077BA] z-10"></div>*/}
         </>
     );
 }
