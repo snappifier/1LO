7fe4130b293d20f34ac51ce8798e4e38fa6b8972
Mitrix05 <78508821+Mitrix05@users.noreply.github.com>
2025-09-16 10:00:29 +0200

    Nowa wersja - poprawiona wersja

--- src/sections/Home/AktualnosciHome.jsx
+++ src/sections/Home/AktualnosciHome.jsx
@@ -20,14 +20,17 @@ export const AktualnosciHome = () => {
 
 
     return (
-        <div className="font-[poppins] flex w-full h-max gap-5 px-[15vw] mt-30 md:gap-20">
-            <div className="flex flex-col items-end w-full h-max">
-                <div className="flex flex-col w-max items-end">
-                    <p className="text-xl font-medium">AKTUALNOŚCI</p>
-                    <p className="text-lg font-light">BĄDŹ NA BIEŻĄCO</p>
+        <div className="font-[poppins] flex w-full h-max gap-5 px-[10vw] md:px-[15vw] mt-40 md:gap-20">
+            <div className="flex flex-col gap-5 items-end w-full h-max">
+                <div className="flex items-center gap-5">
+                    <div className="flex flex-col items-end">
+                        <p className="text-xl font-medium">AKTUALNOŚCI</p>
+                        <p className="text-lg font-light">BĄDŹ NA BIEŻĄCO</p>
+                    </div>
+                    <div className="w-3 h-15 bg-[#3077BA] rounded-2xl drop-shadow-md/20"></div>
                 </div>
                 <div className="w-full flex h-max items-center gap-2">
-                    <div className="md:px-0 md:py-5 px-[10vw] max-w-7xl mx-auto">
+                    <div className="md:px-0 md:py-5 max-w-8xl mx-auto">
                         <div
                             className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-5 max-lg:max-w-3xl max-md:max-w-md mx-auto">
                             {posts.map((news) => (
@@ -53,7 +56,6 @@ export const AktualnosciHome = () => {
                     </div>
                 </div>
             </div>
-            <div className="w-5 h-160 bg-[#3077BA] rounded-2xl"></div>
         </div>
     )
 }
--- src/sections/Home/Wstep.jsx
+++ src/sections/Home/Wstep.jsx
@@ -2,22 +2,25 @@ import Zamoyski_jpg from "../../assets/Home/zamoyski.jpg"
 import {Link} from "react-router-dom";
 
 export default function Wstep() {
-    return <div className="font-[poppins] flex w-full h-max gap-5 px-[15vw] mt-30 md:gap-20">
-        <div className="w-5 h-110 bg-[#3077BA] rounded-2xl drop-shadow-md/20"></div>
-        <div className="flex flex-col w-full h-max">
+    return <div className="font-[poppins] flex flex-col w-full h-max gap-5 md:px-[15vw] px-[10vw] mt-15 md:mt-30 md:gap-15">
+        <div className="flex w-full gap-2 items-center">
+            <div className="w-3 h-10 bg-[#3077BA] rounded-2xl drop-shadow-md/20"></div>
             <p className="text-xl font-medium">KRÓTKO O SZKOLE </p>
-            <div className="w-full flex h-max items-center gap-2">
-                <div className="w-full h-max flex flex-col gap-8">
-                    <p className="w-max font-[meow_script] hidden md:block md:text-5xl lg:text-7xl">Jestem w pierwszym</p>
-                    <p className="font-[montserrat] font-regular w-4/5 text-wrap md:text-xl lg:text-xl ">
-                        I Liceum Ogólnokształcące im. Jana Zamoyskiego w Zamościu – szkoła ponadpodstawowa, mieszcząca się od 1916 roku w gmachu zabytkowej, zbudowanej w stylu wczesnobarokowym, dawnej Akademii Zamojskiej.
-                    </p>
+        </div>
+        <div className="w-full flex h-max items-center gap-2">
+            <div className="w-full h-max flex flex-col gap-10">
+                <p className="w-max font-[meow_script] hidden md:block md:text-5xl lg:text-7xl">Jestem w pierwszym</p>
+                <p className="font-[montserrat] font-regular md:w-4/5 w-full text-wrap md:text-xl lg:text-xl ">
+                    I Liceum Ogólnokształcące im. Jana Zamoyskiego w Zamościu – szkoła ponadpodstawowa, mieszcząca się od 1916 roku w gmachu zabytkowej, zbudowanej w stylu wczesnobarokowym, dawnej Akademii Zamojskiej. 6 roku w gmachu zabytkowej, zbudowanej w stylu wczesnobarokowym, dawnej Akademii Zamojskiej.
+                </p>
+                <img className="md:hidden w-auto mr-5" src={Zamoyski_jpg} alt="Zamoyski" />
+                <div className="flex w-full justify-center md:justify-start">
+                    <div className="flex justify-center items-center w-50 h-14 bg-[#3077BA] rounded-xl drop-shadow-md/20">
+                        <p className="font-normal text-white">Dowiedz się więcej</p>
+                    </div>
                 </div>
-                <img className="w-auto mr-5" src={Zamoyski_jpg} alt="Zamoyski" />
             </div>
-            <div className="flex justify-center items-center w-50 h-14 bg-[#3077BA] rounded-xl drop-shadow-md/20">
-                <p className="font-normal text-white">Dowiedz się więcej</p>
-            </div>
+            <img className="hidden md:block mr-5" src={Zamoyski_jpg} alt="Zamoyski" />
         </div>
     </div>
 }
