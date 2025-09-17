d75dc9c1f9aa66d91bf7800e342b7cfc66d53ebc
snappify <nzzhry@gmail.com>
2025-09-17 12:49:07 +0200

    dodanie insight

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { injectSpeedInsights } from '@vercel/speed-insights';
 import {BrowserRouter} from "react-router";
 import {AppRoutes} from "./AppRoutes";
 import {Suspense} from "react";
 import {Loader} from "./components/animations/Loader.jsx";
 import { Analytics } from "@vercel/analytics/react"
 
-injectSpeedInsights();
+
 
 function App() {
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
       <QueryClientProvider client={queryClient}>
           <App />
+          <SpeedInsights />
       </QueryClientProvider>
   </StrictMode>,
 )
