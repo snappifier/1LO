75799b1f61e10869b579034d9e7850c6de4f71ae
Mitrix05 <78508821+Mitrix05@users.noreply.github.com>
2025-09-17 13:26:25 +0200

    API UPDATE

--- src/features/fetcher.jsx
+++ src/features/fetcher.jsx
@@ -1,10 +1,14 @@
 const API_URL = import.meta.env.VITE_API_URL;
+import blank_photo from "../assets/Home/zamoyski.jpg"
 
 export function get(endpoint){
-    console.log('API URL:', API_URL);
     return fetch(API_URL + "/api/" + endpoint).then(response => response.json());
 }
 
 export function getStrapiMedia(url) {
-    return `${API_URL}${url}`;
+    if (url !== undefined) {
+
+        return `${API_URL}${url}`;
+    }
+    return blank_photo;
 }
