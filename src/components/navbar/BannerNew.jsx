3a642e792624b4d04d6664d432ad05676ab40830
Mitrix05 <78508821+Mitrix05@users.noreply.github.com>
2025-09-17 17:33:05 +0200

    Optimalizacja zdjęć

--- src/components/navbar/BannerNew.jsx
+++ src/components/navbar/BannerNew.jsx
@@ -8,7 +8,7 @@ export const BannerNew = () => {
     <header className="fixed top-0 w-full h-30 flex flex-col items-center justify-center z-100">
         <div className="w-[80%] h-15 bg-[#3077BA]/80 backdrop-blur-xs backdrop-saturate-300 rounded-xl flex items-center justify-between px-10 shadow-lg">
             <Link to={'/'} className="z-[60]">
-            <motion.img src={images["logo"]} width={64} height={64} style={{ maxInlineSize: '100%' }}  alt="logo" className="h-11" whileHover={{scale: 1.1}} whileTap={{scale: 1}}/>
+            <motion.img src={images["logo"]} style={{ maxInlineSize: '100%' }}  alt="logo" className="h-11 w-11" whileHover={{scale: 1.1}} whileTap={{scale: 1}}/>
             </Link>
             <div className="flex items-center ">
                 <DropdownNew />
