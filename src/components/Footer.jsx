ab0593800305513737d6cbd9f91d13f0676376d0
snappify <nzzhry@gmail.com>
2025-09-17 16:50:32 +0200

    shift fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Menu from "./footer/menu.jsx";
 export default function Footer(){
     return <>
         <footer className="flex flex-col z-40  md:items-center text-white pt-4 overflow-hidden whitespace-nowrap w-full h-full bg-[#3077BA]">
-            <Slider />
+            {/*<Slider />*/}
             <Menu />
         </footer>
     </>
