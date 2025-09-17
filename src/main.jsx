e9d887223ae924b89c91fc552a4112ba8e77f02b
snappify <nzzhry@gmail.com>
2025-09-17 13:45:57 +0200

    main fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,17 @@
-import React, { StrictMode, Suspense, lazy } from 'react';
-import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import './index.css';
 import App from './App.jsx';
 
 const queryClient = new QueryClient();
 
-const LazySpeedInsights = lazy(() =>
-    import('@vercel/speed-insights/react').then(mod => ({ default: mod.SpeedInsights }))
-);
-
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <App />
-            <Suspense fallback={null}>
-                <LazySpeedInsights />
-            </Suspense>
+            <SpeedInsights/>
         </QueryClientProvider>
     </StrictMode>
 );
