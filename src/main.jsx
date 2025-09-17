de58d962119d994d2709c4645a9407a48fc6c5d6
snappify <nzzhry@gmail.com>
2025-09-17 12:50:55 +0200

    dodanie insight

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
       <QueryClientProvider client={queryClient}>
           <App />
+
           <SpeedInsights />
       </QueryClientProvider>
   </StrictMode>,
