efc3e989720371ef6ca704895e2bab9368236e67
Mitrix05 <78508821+Mitrix05@users.noreply.github.com>
2025-09-20 17:55:47 +0200

    Wstep - poprawki mobile

--- src/sections/Home/banner.jsx
+++ src/sections/Home/banner.jsx
@@ -14,7 +14,7 @@ export default function Banner() {
                 style={{ maxInlineSize: '100%' }}
                 fetchPriority="high"
                 loading="eager"
-                decoding="async"
+                decoding="sync"
 
             />
 
@@ -26,13 +26,13 @@ export default function Banner() {
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;w Pierwszym”
             </p>
         </div>
-            <div className="hidden md:relative md:flex justify-between md:-bottom-[calc(0vh-45px)] left-1/2 translate-x-[-50%] min-w-80 max-w-280 gap-8 h-[90px] px-10">
+            <div className="flex md:relative md:flex justify-between md:-bottom-[calc(0vh-45px)] left-1/2 translate-x-[-50%] min-w-80 max-w-280 gap-8 h-[90px] px-10">
                 <motion.div
                     whileHover={{scale: 1.05}}
                     transition={{ duration: 0.2, ease: "easeOut" }}
-                    className="bg-[#3077BA] h-full w-full drop-shadow-lg/30 rounded-xl flex flex-col justify-center items-center text-white cursor-pointer ">
+                    className="bg-[#3077BA] h-full w-full drop-shadow-lg/30 rounded-xl flex flex-col justify-center items-center text-white cursor-pointer text-wrap">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="M12 12q-1.65 0-2.825-1.175T8 8t1.175-2.825T12 4t2.825 1.175T16 8t-1.175 2.825T12 12m-8 6v-.8q0-.85.438-1.562T5.6 14.55q1.55-.775 3.15-1.162T12 13t3.25.388t3.15 1.162q.725.375 1.163 1.088T20 17.2v.8q0 .825-.587 1.413T18 20H6q-.825 0-1.412-.587T4 18"/></svg>
-                    <p className="text-xl font-light select-none" >Panel nauczyciela</p>
+                    <p className="text-xl font-light select-none text-center" >Panel nauczyciela</p>
                 </motion.div>
                 <motion.div
                     whileHover={{scale: 1.05}}
