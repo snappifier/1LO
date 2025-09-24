8e81dc53d2990f571b5abb804e13f3d79f56e4f5
Snappify <nzzhry@gmail.com>
2025-09-24 18:50:50 +0200

    statut

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,6 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useEffect, lazy, Suspense } from "react";
 import { ScrollToTop } from "./features/ScrollToTop.jsx";
+import {useSuspenseQuery} from "@tanstack/react-query";
+import {Template} from "./components/template-1.jsx";
+import {get} from "./features/fetcher.jsx";
+
+
 import Home from "./sections/Home/Home.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import { NavbarNew } from "./components/navbar/NavbarNew.jsx";
@@ -8,9 +13,9 @@ import { Maintenance } from "./components/Maintenance.jsx";
 import { Loader } from "./components/animations/Loader.jsx";
 import Kadra from "./sections/Szkoła/Kadra.jsx";
 import Dyrektorzy from "./sections/Szkoła/Dyrektorzy.jsx";
-import {Template} from "./components/template-1.jsx";
-import {get} from "./features/fetcher.jsx";
-import {useSuspenseQuery} from "@tanstack/react-query";
+import Statut from "./sections/Dokumenty/Statut.jsx"
+
+
 
 const Aktualnosci = lazy(() => import("./sections/Aktualności/Aktualnosci.jsx"));
 const Post = lazy(() => import("./components/Post.jsx"));
@@ -36,7 +41,7 @@ export const AppRoutes = () => {
                     <Route path="/post/:id" element={<Post />} />
                     <Route path="/kadra" element={<Kadra />} />
                     <Route path="/dyrektorzy" element={<Dyrektorzy />} />
-                    <Route path="/nav" element={<NavbarNew />} />
+                    <Route path="/statut" element={<Statut />} />
                     {uczniowieLinks.map((item) => {
                         if(item["Template"] === "Main"){
                             return <Route path={item["Link"]} element={<Template />} />;
---
+++ src/sections/Dokumenty/Statut.jsx
@@ -0,0 +1,39 @@
+import {motion} from "motion/react";
+import {Link} from "react-router-dom";
+import statut from "../../assets/2025_Statut_ILO.pdf"
+
+
+const Statut = () => {
+
+
+    return (
+        <>
+            <div className="w-full pt-36 md:pt-40 pb-16 md:pb-20 flex flex-col items-center h-[80vh]">
+                <div className="w-[92%] sm:w-[90%] lg:w-[80%]">
+                        <div className="w-full flex flex-col mb-4 sm:mb-6">
+                            <p className="text-3xl sm:text-4xl lg:text-5xl font-extralight w-max">
+                                Statut
+                            </p>
+                        </div>
+                    <div className="w-max h-max">
+                        <Link to={"http://1lo.com.pl/pdf/2025_Statut_ILO.pdf"} target={"_blank"} className="w-max h-max">
+                            <motion.div
+                                className="w-max cursor-pointer flex items-center justify-between gap-3"
+                                whileHover={{scale: 1.02}}
+                                transition={{type: "spring", stiffness: 260, damping: 24}}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24">
+                                    <path fill="#959595"
+                                          d="M12 15.575q-.2 0-.375-.062T11.3 15.3l-3.6-3.6q-.3-.3-.288-.7t.288-.7q.3-.3.713-.312t.712.287L11 12.15V5q0-.425.288-.712T12 4t.713.288T13 5v7.15l1.875-1.875q.3-.3.713-.288t.712.313q.275.3.288.7t-.288.7l-3.6 3.6q-.15.15-.325.213t-.375.062M6 20q-.825 0-1.412-.587T4 18v-2q0-.425.288-.712T5 15t.713.288T6 16v2h12v-2q0-.425.288-.712T19 15t.713.288T20 16v2q0 .825-.587 1.413T18 20z"/>
+                                </svg>
+                                <p className="text-xl mt-1 font-[poppins] font-light text-slate-700">Pobierz statut szkoły</p>
+                            </motion.div>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Statut;
