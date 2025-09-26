ed242bf9a18476f7fab9024f4f78486514f0fc82
Snappify <snappifyacc@gmail.com>
2025-09-26 15:59:52 +0200

    footer fix

--- src/components/footer/FooterNew.jsx
+++ src/components/footer/FooterNew.jsx
@@ -120,7 +120,7 @@ const FooterNew = () => {
                 <div className="w-full h-max flex items-center justify-center py-2">
                     <div className="max-w-[80%] w-full h-max flex items-center justify-center gap-5 py-2">
                     <motion.img src={images?.["logo_thumbnail"] ? images["logo_thumbnail"] : logo}  alt="logo" className="select-none h-15 w-15   drop-shadow-lg/20" whileHover={{scale: 1.1}} whileTap={{scale: 1}}/>
-                    <p className=" text-center text-gray-100 font-[poppins] text-xs sm:text-xs lg:text-md max-w-2/3 font-light whitespace-normal break-words hyphens-auto">I Liceum Ogólnokształcące im. Jana Zamoyskiego w Zamościu </p>
+                    <p className=" text-center text-gray-200 font-[poppins] text-xs sm:text-xs lg:text-md max-w-2/3 font-light whitespace-normal break-words hyphens-auto">I Liceum Ogólnokształcące im. Jana Zamoyskiego w Zamościu </p>
 
                         <img src={images?.["godlo_thumbnail"] ? images["godlo_thumbnail"] : godlo} alt="Godło" className=" select-none h-15 w-auto object-contain" />
                     </div>
@@ -135,7 +135,7 @@ const FooterNew = () => {
                         <Link
                             key={item.name}
                             to={item.href}
-                            className="text-gray-100 hover:text-white select-none"
+                            className="text-gray-200 hover:text-white select-none"
                         >
                             {item.name}
                         </Link>
